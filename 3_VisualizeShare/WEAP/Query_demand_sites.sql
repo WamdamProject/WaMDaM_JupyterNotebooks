1d5bbd78787c162a5c41481b2ef8753266b17bc1
Adel M. Abdallah <amabdallah@aggiemail.usu.edu>
2018-11-27 11:17:16 -0700

    Create Query_demand_sites.sql

---
+++ 3_VisualizeShare/WEAP/Query_demand_sites.sql
@@ -0,0 +1,60 @@
+/*
+2_IdentifyDemandSites_Seasonal_Values.sql
+*/
+
+Select DISTINCT ResourceTypeAcronym,ScenarioName,ObjectTypeCV,AttributeName_Abstract,AttributeNameCV,UnitName,
+InstanceName,SeasonName,SeasonOrder,SeasonNumericValue
+
+
+FROM ResourceTypes
+
+Left JOIN "ObjectTypes" 
+ON "ObjectTypes"."ResourceTypeID"="ResourceTypes"."ResourceTypeID"
+
+-- Join the Objects to get their attributes  
+LEFT JOIN  "Attributes"
+ON "Attributes"."ObjectTypeID"="ObjectTypes"."ObjectTypeID"
+
+LEFT JOIN "Mappings"
+ON "Mappings"."AttributeID"= "Attributes"."AttributeID"
+
+LEFT JOIN "Instances" 
+ON "Instances"."InstanceID"="Mappings"."InstanceID"
+
+LEFT JOIN "InstanceCategories" 
+ON "InstanceCategories"."InstanceCategoryID"="Instances"."InstanceCategoryID"
+
+LEFT JOIN "ValuesMapper" 
+ON "ValuesMapper"."ValuesMapperID"="Mappings"."ValuesMapperID"
+
+LEFT JOIN "ScenarioMappings"
+ON "ScenarioMappings"."MappingID"="Mappings"."MappingID"
+
+LEFT JOIN "Scenarios" 
+ON "Scenarios"."ScenarioID"="ScenarioMappings"."ScenarioID"
+
+LEFT JOIN "MasterNetworks" 
+ON "MasterNetworks"."MasterNetworkID"="Scenarios"."MasterNetworkID"
+
+LEFT join "Methods"
+ ON "Methods"."MethodID" = "Mappings"."MethodID"
+
+LEFT join "Sources" 
+ON "Sources"."SourceID" = "Mappings"."SourceID"
+
+
+-- Join the DataValuesMapper to get their Seasonal 
+LEFT JOIN "SeasonalNumericValues"
+ON "SeasonalNumericValues"."ValuesMapperID" = "ValuesMapper"."ValuesMapperID" 
+
+
+WHERE AttributeName_Abstract ='Monthly Demand'
+
+
+AND ScenarioName=='Bear River WEAP Model 2017'
+
+AND AttributeDataTypeCV IN ('SeasonalNumericValues')
+
+AND InstanceName in ('Logan Potable','North Cache Potable','South Cache Potable')
+
+GROUP  BY ScenarioName, InstanceName,SeasonOrder
